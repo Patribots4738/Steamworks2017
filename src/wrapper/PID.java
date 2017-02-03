cc9cb74ea625e4f6ce866eec1ed01b784a259600
RT9 <seanmm11323@gmail.com>
2017-02-02 17:35:09 -0800

    added PID class

---
+++ src/wrapper/PID.java
@@ -0,0 +1,24 @@
+package wrapper;
+
+public class PID {
+	
+	double error=0;
+	
+	public double CalcP(double SP, double PV){
+		
+		return 0;
+	}
+	
+	public double CalcI(double SP, double PV){
+		
+		return 0;
+	}
+	
+	public double CalcD(double SP, double PV){
+		
+		return 0;
+	}
+	
+	
+}
+
