88343ab1d9534164d34adfaaa140a9f48df4d33d
RT9 <seanmm11323@gmail.com>
2017-02-28 18:28:07 -0800

    Changing autonomous

--- src/org/usfirst/frc/team4738/robot/Robot.java
+++ src/org/usfirst/frc/team4738/robot/Robot.java
@@ -15,57 +15,53 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
+	//Cheeki Breeki
 	PIDVictorSP[] motors;
 	VictorSP wench;
 	Gyro gyro;
 	PIDMecanumDrive drive;
+	Autonomous autoDrive;
 	// Camera cam;
 
 	XboxController xbox;
 	Gamepad pad;
-	Arms arm;
+	Arms arms;
 	Kicker kicker;
 
-	// this is what we're using to test the code
 	public void robotInit() {
-		arm = new Arms(0, 1);
-		kicker = new Kicker(2, 3);
-
-		gyro = new Gyro(1);
-
+		//arms = new Arms(0, 1);
+		//kicker = new Kicker(2, 3);
+		arms = new Arms(4);
+		kicker = new Kicker(5);
+		gyro = new Gyro(0);
+		
 		motors = new PIDVictorSP[4];
-		motors[0] = new PIDVictorSP(7, 2, 3, 3, 0, 0, 0); // front left
+		/*motors[0] = new PIDVictorSP(7, 2, 3, 3, 0, 0, 0); // front left
 		motors[1] = new PIDVictorSP(8, 6, 7, 3, 0, 0, 0);// back left
 		motors[2] = new PIDVictorSP(6, 0, 1, 3, 0, 0, 0);// front right
 		motors[3] = new PIDVictorSP(9, 4, 5, 3, 0, 0, 0);// back right
-		drive = new PIDMecanumDrive(3, 0, 0, 0, motors);
-		// drive = new PIDMecanumDrive(4, 0, 0, 0, 0, 1, 2, 3);
+		drive = new PIDMecanumDrive(3, 0, 0, 0, motors);*/
+		drive = new PIDMecanumDrive(4, 0, 0, 0, 0, 1, 2, 3);
 		xbox = new XboxController(0);
 		pad = new Gamepad(1);
 		wench = new VictorSP(5);
 		// pad = new XboxController(0);
 		Camera cam = new Camera();
 		//cam.startCamera();
-
-		System.out.println("PRINTING STUFFS");
-		try {
-			File dir = new File("/dev/");
-			File[] filesList = dir.listFiles();
-			for (File file : filesList) {
-				if (file.isFile()) {
-					System.out.println(file.getName());
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("Yeah, the file listing crashed.");
-		}
+		autoDrive = new Autonomous(drive, gyro, arms, kicker);
+		
+		SmartDashboard.putNumber("Gyro", gyro.getAngle());
 	}
 
 	public void autonomousInit() {
+		autoDrive.reset(); 
 	}
 
 	public void autonomousPeriodic() {
-
+		//autoDrive.autonomousChooser(0);
+		//autoDrive.rotate(15, 0.25, 0);
+		autoDrive.stop(1);
+		SmartDashboard.putNumber("Gyro", gyro.getAngle());
 	}
 
 	public void teleopPeriodic() {
@@ -73,10 +69,10 @@ public class Robot extends IterativeRobot {
 
 		wench.set(pad.getAxis(1));
 
-		arm.openArms(pad.getButton(1));
+		arms.openArms(pad.getButton(1));
 		kicker.openKicker(pad.getButton(0));
 
-		SmartDashboard.putString("Gyro angle", "" + gyro.getAngle());
+		SmartDashboard.putString("Gyro", "" + gyro.getAngle());
 		// cam.camUpdate();
 	}
 
