278476282dd60df295cb36f3b486e5cc9da91388
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2017-02-28 18:27:21 -0800

    Wrote untested vision code in java

---
+++ src/org/usfirst/frc/team4738/wrapper/vision/VisionObject.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team4738.wrapper.vision;
+
+public class VisionObject {
+	public double distanceMagnitude; //The Hypotnuse of distance
+	public double angleFromCamera;
+	public double[] distance; //The distance in Horizontal and Forwards coordinates from the camera
+}
