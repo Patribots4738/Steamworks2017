b31cd74425e6090bf842df65de7cb0353f261796
BUNN1E5 <BUNN1E5@users.noreply.github.com>
2017-03-20 23:47:07 -0700

    Added some comments to the vision code
    It's now easier to figure out what is happening in the program even if it's just a general overview

--- src/org/usfirst/frc/team4738/wrapper/vision/ObjectDetector.java
+++ src/org/usfirst/frc/team4738/wrapper/vision/ObjectDetector.java
@@ -20,28 +20,32 @@ public class ObjectDetector {
 	
 	public ObjectDetector(double focalLength, double actualHeight, double FOV, int erode_size, int dialate_size, Scalar upper, Scalar lower){
 		this.focalLength = focalLength;
-		this.actualHeight = actualHeight;
+		this.actualHeight = actualHeight; //This is the height of the object you are trying to detect
 		this.FOV = FOV;
 		erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(2*erode_size + 1, 2*erode_size+1));
 		dialateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(2*dialate_size + 1, 2*dialate_size+1));
 	}
 	
+	//This finds distance using triangle similarity, you can find a useful doc in vision equations 
 	public VisionObject[] findObjects(Mat mat){
 		
-		Core.flip(mat, mat, 1);
+		Core.flip(mat, mat, 1); //Flips the image horizontally so it isn't mirrored
 		
 		Mat src = mat.clone(), dst = mat.clone();
-		Mat hierarchy = new Mat();
-		Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2HSV);
-		Imgproc.GaussianBlur(dst, dst, new Size(5, 5),  2, 2);
+		Mat hierarchy = new Mat(); 
+		Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2HSV); //Converts the image from RGB to HSV color space
+		Imgproc.GaussianBlur(dst, dst, new Size(5, 5),  2, 2); //Surprise! This blur blurs the image
 		
-		Core.inRange(dst, lowerBound, upperBound, dst);
+		//This return a binary image(black and white) of the pixels that have a HSV value between the upper and lower bound
+		Core.inRange(dst, lowerBound, upperBound, dst); 
 		
-		Imgproc.erode(dst, dst, erodeElement);
-		Imgproc.dilate(dst, dst, dialateElement);	
+		Imgproc.erode(dst, dst, erodeElement); //This removes any small noise by encroachin in on all of the pixels
+		Imgproc.dilate(dst, dst, dialateElement); //We then expand all of the pixels to fix the eroding we did, but now the noise is gone
 		
 		ArrayList<MatOfPoint> contours = new ArrayList<>();
 		
+		//Here we are finding enclosed objects
+		//Warning this is pretty expensive
 		Imgproc.findContours(dst.clone(), contours, hierarchy,  Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
 		ArrayList<VisionObject> objects = new ArrayList<>(contours.size());
 		
@@ -49,15 +53,18 @@ public class ObjectDetector {
 			VisionObject obj = new VisionObject();
 			RotatedRect rect = new RotatedRect();
 			rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
-			double distanceForwards = (actualHeight * focalLength) / (rect.size.height);
-			double distanceHorizontal = rect.center.x * Math.sin(Math.toRadians(FOV/2));
-			obj.angleFromCamera = Math.atan2(distanceForwards, distanceHorizontal);
-			double[] distance = {distanceForwards, distanceHorizontal};
+			double distanceForwards = (actualHeight * focalLength) / (rect.size.height); //Calculates the distance using triangle similarity
+			double distanceHorizontal = rect.center.x * Math.sin(Math.toRadians(FOV/2)); //Incomplete calculation to determine the horizontal position of the object
+			obj.angleFromCamera = Math.atan2(distanceForwards, distanceHorizontal); //Angle from camera because I though I needed it when I wrote it (WTF?)
+			double[] distance = {distanceForwards, distanceHorizontal}; //I'm sure you can figure out what this is doing...
 			obj.distance = distance;
-			obj.distanceMagnitude = Math.sqrt((distanceHorizontal * distanceHorizontal) + (distanceForwards * distanceForwards)); 
+			obj.distanceMagnitude = Math.sqrt((distanceHorizontal * distanceHorizontal) + (distanceForwards * distanceForwards)); //Our lord pythagoras with his great theorem
 			
 			objects.add(obj);
 		}
+		
+		//In case you are wondering, this is how you convert an arraylist to an array
+		//It's => ArrayList.toArray(new Type[ArrayList.size()]);
 		
 		return objects.toArray(new VisionObject[objects.size()]);
 	}
